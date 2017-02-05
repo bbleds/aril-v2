64345eacfbe4aca10e1552e7f2bc7294dad1fb7f
Ben Bledsoe <Ben Bledsoe>
2017-02-04 19:44:37 -0600

    Init gulp task flow and project structure

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const cleanCss = require('gulp-clean-css');
+
+// concat, babelify, and minify js
+gulp.task("babel", () => {
+  return gulp.src("src/js/*.js")
+    .pipe(concat('scripts.min.js'))
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(gulp.dest("build/js"));
+});
+
+// concat, compile, and minify sass to css
+gulp.task('sass', () => {
+  return gulp.src('src/sass/*.scss')
+    .pipe(concat('styles.min.css'))
+    .pipe(sass())
+    .pipe(cleanCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('build/css'));
+});
+
+// handle watchers
+gulp.task('watch', () => {
+  gulp.watch('src/sass/*.scss', ['sass']);
+  gulp.watch('src/js/*.js', ['babel']);
+})
+
+// main task
+gulp.task("default", ['babel','sass','watch']);
